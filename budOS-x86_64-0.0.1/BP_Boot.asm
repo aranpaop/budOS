cd7c98ad82c8f7541b021f14c80c087fa3972a6f
aranpaop <paopstory@outlook.com>
2020-03-09 00:41:53 +0800

    v0.0.1

---
+++ budOS-x86_64-0.0.1/BP_Boot.asm
@@ -0,0 +1,36 @@
+    org 0x7c00
+
+    jmp Label_BootStart
+    nop
+
+%include "BP_CommonDefinitions.inc"
+%include "BP_BootLoaderUtils.inc"
+
+BaseOfStack       equ 0x7c00
+BaseOfLoader      equ 0x1000
+OffsetOfLoader    equ 0
+TempBufferAddress equ 0x8000
+
+RootDirSectors          equ 14
+SectorNumOfRootDirStart equ 19
+SectorNumOfFAT1Start    equ 1
+SectorBalance           equ 17
+
+BootStartMessage db "Start Boot..."
+
+Label_BootStart:
+    mov sp, BaseOfStack
+
+    call BPFunc_ClearScreen
+
+    mov dx, 0
+    call BPFunc_SetFocus
+
+    mov bp, BootStartMessage
+    mov cx, Label_BootStart - BootStartMessage
+    call BPFunc_DisplayString
+
+    jmp $
+
+    times 510 - ($ - $$) db 0
+    dw 0xaa55
